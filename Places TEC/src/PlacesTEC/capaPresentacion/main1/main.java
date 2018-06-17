228f4840fd26795def89f11e8cd097d558528271
sebas <sebas@Sebas>
2018-06-17 12:59:40 -0600

    Progra 3

--- Places TEC/src/PlacesTEC/capaPresentacion/main1/main.java
+++ Places TEC/src/PlacesTEC/capaPresentacion/main1/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package progra.pkg3;
+package PlacesTEC.capaPresentacion.main1;
 
 import PlacesTEC.capaLogica.estructura.BST;
 import PlacesTEC.capaLogica.estructura.NodoBST;
